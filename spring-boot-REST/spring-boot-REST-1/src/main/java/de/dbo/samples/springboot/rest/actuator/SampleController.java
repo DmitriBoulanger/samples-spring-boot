1f1e8ded2364491fe8965a02a25b73f7335378ce
Dmitri Boulanger <Dmitri Boulanger@Hombach-PC>
2016-07-26 22:51:22 +0200

    INIT

---
+++ spring-boot-REST/spring-boot-REST-1/src/main/java/de/dbo/samples/springboot/rest/actuator/SampleController.java
@@ -0,0 +1,65 @@
+package de.dbo.samples.springboot.rest.actuator;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Description("A controller for handling requests for hello messages")
+public class SampleController {
+
+	@Autowired
+	private HelloWorldService helloWorldService;
+
+	@GetMapping("/")
+	@ResponseBody
+	public Map<String, String> hello() {
+		return Collections.singletonMap("message",
+				this.helloWorldService.getHelloMessage());
+	}
+
+	@PostMapping("/")
+	@ResponseBody
+	public Map<String, Object> olleh(@Validated Message message) {
+		Map<String, Object> model = new LinkedHashMap<String, Object>();
+		model.put("message", message.getValue());
+		model.put("title", "Hello Home");
+		model.put("date", new Date());
+		return model;
+	}
+
+	@RequestMapping("/foo")
+	@ResponseBody
+	public String foo() {
+		throw new IllegalArgumentException("Server error");
+	}
+
+	protected static class Message {
+
+		@NotBlank(message = "Message value cannot be empty")
+		private String value;
+
+		public String getValue() {
+			return this.value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+	}
+
+}
+
