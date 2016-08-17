648948edbb80bab5a1782029212645a80ad4bda2
Dmitri Boulanger <Dmitri Boulanger@Hombach-PC>
2016-08-17 20:22:48 +0200

    app3 with app-reactor

---
+++ spring-boot-JBEHAVE/spring-boot-JBEHAVE-2/spring-boot-JBEHAVE-2-app/spring-boot-JBEHAVE-2-app-3/src/main/java/de/dbo/samples/springboot/jbehave2/app3/service/GreetingController.java
@@ -0,0 +1,59 @@
+package de.dbo.samples.springboot.jbehave2.app3.service;
+
+import de.dbo.samples.springboot.jbehave2.app3.domain.Customer;
+import de.dbo.samples.springboot.jbehave2.app3.domain.CustomerRepository;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class GreetingController {
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong    counter  = new AtomicLong();
+
+    @Autowired
+    CustomerRepository          customerRepository;
+    
+    public GreetingController() {
+	log.info("Greeting service-controller created");
+	log.error("ok");
+    }
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody Greeting sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
+
+	final long repositoryCount = customerRepository.count() ;
+	log.info("Repository count : " + repositoryCount);
+	if (0==repositoryCount) {
+	    log.error("No data in the customer repository: Elastic search repository was not loaded?");
+	} 
+	else {
+	    final List<Customer> customers = customerRepository.findByLastName("Smith");
+	    final StringBuilder sb = new StringBuilder("Repository count " +repositoryCount + ": ");
+	    for (Customer customer:customers) {
+		sb.append("\n\t - ID=" + customer.getId() + " " + customer.getFirstName());
+	    }
+	    log.info(sb.toString());
+	}
+	
+	if (null!=name && name.equals("XXX")) {
+	    log.error("Bad name:", new Exception("Bad-name exception just to see it"));
+	}
+
+	final Greeting greeting =  new Greeting(counter.incrementAndGet(), String.format(template, name));
+	return greeting;
+    }
+
+}
---
+++ spring-boot-JBEHAVE/spring-boot-JBEHAVE-2/spring-boot-JBEHAVE-2-app/spring-boot-JBEHAVE-2-app-3/src/test/java/de/dbo/samples/springboot/jbehave2/app3/GreetingTest.java
@@ -0,0 +1,110 @@
+package de.dbo.samples.springboot.jbehave2.app3;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+//
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+//
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Basic integration tests for service demo application.
+ *
+ * @author Dmitri Boulanger, Hombach
+ *
+ * D. Knuth: Programs are meant to be read by humans and
+ *           only incidentally for computers to execute
+ *
+ */
+//
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = {GreetingTestApplication.class, GreetingTestConfiguration.class})
+@DirtiesContext
+//
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@SuppressWarnings("rawtypes")
+public class GreetingTest {
+    private static final Logger log = LoggerFactory.getLogger(GreetingTest.class);
+
+    @LocalServerPort
+    private int                 port;
+
+    @Test
+    public void test00_Greeting() throws Exception {
+
+        final TestRestTemplate restTemplate = new TestRestTemplate();
+        final URI uri = toURI(HELLO);
+        log.info("request " + uri + " ...");
+        final ResponseEntity<Map> entity = restTemplate.getForEntity(uri, Map.class);
+        assertThatHttpStatus(HttpStatus.OK, entity);
+    }
+
+    @Test
+    public void test10_Admin() throws Exception {
+        final TestRestTemplate restTemplate = new TestRestTemplate();
+        final URI uri = toURI(INFO);
+        log.info("request " + uri + " ...");
+        final ResponseEntity<Map> entity = restTemplate.getForEntity(uri, Map.class);
+        assertThatHttpStatus(HttpStatus.OK, entity);
+    }
+
+    // ========================
+    // Test-specific assertions
+    // ========================
+
+    private static final void assertThatHttpStatus(final HttpStatus expectedHttpStatus, final ResponseEntity<Map> entity) {
+        final HttpStatus responseHttpStatus = entity.getStatusCode();
+        assertThat("HTTP response code is not as expected", expectedHttpStatus, equalTo(responseHttpStatus));
+    }
+
+    // ========================
+    // Test implementations
+    // ========================
+
+    private static final String HELLO = "hello-world";
+    private static final String INFO  = "info";
+
+    private final URI toURI(final String path) {
+        try {
+            switch (path) {
+
+                case HELLO:
+                    return new URI("http://" + "localhost" + ":" + this.port + "/" + path);
+
+                case INFO:
+                    return new URI("http://localhost:" + this.port + "/" + path);
+
+                default:
+                    throw new IllegalArgumentException("Path [" + path + "] is unknown");
+            }
+
+        }
+        catch(URISyntaxException e) {
+            throw new IllegalStateException("Should never happen error: non-parsable hard-coded URI");
+        }
+    }
+
+    // =======================
+    //   HELPERS
+    // =======================
+
+}
